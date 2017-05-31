23a50b87c338301faa92e681d9c1b7c5557589bd
Sam Marder <sam@marder.co>
2017-05-30 23:13:43 -0400

    Migrated to FRED.

--- prime.py
+++ prime.py
@@ -6,28 +6,19 @@ from tabulate import tabulate
 data = []
 
 def parse_row(row):
-    if len(row) != 3:
-        return None
+    date_string, rate = row
 
-    date_string, rate, _ = row
-    if len(date_string) == 0 or len(rate) == 0:
-        return None
-
-    try:
-        date = datetime.strptime(date_string, '%d-%b-%y')
-        # Fix ambiguous 1900/2000 2-digit dates.
-        if date.year > datetime.now().year:
-            date = date.replace(year=date.year - 100)
-        rate = float(rate)
-    except TypeError:
-        return None
+    date = datetime.strptime(date_string, '%Y-%m-%d')
+    rate = float(rate)
 
     return date, rate
 
-def load_data():
+def load_data(filename):
     data = []
-    with open('prime.csv') as f:
+    with open(filename) as f:
         reader = csv.reader(f)
+        # Skip header
+        next(reader)
         for row in reader:
             parsed_row = parse_row(row)
             if parsed_row is not None:
@@ -36,6 +27,7 @@ def load_data():
 
 def avg(rows):
     values = [r[1] for r in rows]
+    # Actually use the first value of every year instead of the average.
     # return sum(values) / len(values)
     return values[0]
 
@@ -56,17 +48,7 @@ def normalize_dates(data):
             same_year_rows.append(row)
     normal_data.append((same_year_rows[0][0], avg(same_year_rows)))
 
-    # Fill in missing rows.
-    really_normal_data = [normal_data[0]]
-    i = 1
-    while i < len(normal_data):
-        while really_normal_data[-1][0].year + 1 != normal_data[i][0].year:
-            really_normal_data.append(list(really_normal_data[-1]))
-            new_date = really_normal_data[-1][0].replace(year=really_normal_data[-1][0].year + 1)
-            really_normal_data[-1][0] = new_date
-        really_normal_data.append(normal_data[i])
-        i += 1
-    return really_normal_data
+    return normal_data
 
 def calc_moving_averages(data, years):
     if len(data) == 0:
@@ -90,12 +72,11 @@ def max_averages(averages):
     diffs.sort(reverse=True)
     return averages[diffs[0][1]]
 
-def main():
-    YEARS = 5
-    data = load_data()
+def main(filename, years):
+    data = load_data(filename)
     data.sort()
     data = normalize_dates(data)
-    averages = calc_moving_averages(data, YEARS)
+    averages = calc_moving_averages(data, years)
 
     max_average = max_averages(averages)
 
@@ -104,10 +85,6 @@ def main():
         avg['End Date'] = avg['End Date'].year
 
     print(tabulate(averages, headers='keys', floatfmt=".2f"))
-    print()
-    print('Max %d-year positive difference: %.2f%% (from %d to %d)' % (YEARS, max_average['Difference'], max_average['Start Date'], max_average['End Date']))
-
-
 
 if __name__ == '__main__':
-    main()
+    main(argv[1], argv[2])
